e7b8a4b823266fa864c1975c3c2af9411ce5aabc
Emmanuel Trevino <etrevino8733@gmail.com>
2024-05-24 12:27:31 -0500

    updates

--- scripts/tech-stack.js
+++ scripts/tech-stack.js
@@ -3,7 +3,6 @@ import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.164.1/build/three.m
 
 // import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.164.1/examples/jsm/controls/OrbitControls.js';
 import { MMDPhysics } from 'https://cdn.jsdelivr.net/npm/three@0.164.1/examples/jsm/animation/MMDPhysics.js';
-// import * as Clock from 'https://cdn.jsdelivr.net/npm/three@0.164.1/src/core/Clock.js';
 import { OrbitControls } from '../libraries/Three/OrbitControls.js';
 
 import { FontLoader } from '../libraries/Three/FontLoader.js';
@@ -14,14 +13,14 @@ import { TextGeometry } from '../libraries/Three/TextGeometry.js';
 let timeElapsed = 0;
 let APP_ = null;
 let previousRAF_ = null;
+const DEFAULT_MASS = 10;
 
-// let stack = ["HTML", "CSS", "JavaScript"];
-// let startXPos = -3;
 
 const scene = new THREE.Scene();
 const canvas = document.querySelector('#tech-stack');
 const renderer = new THREE.WebGLRenderer({
     canvas: document.querySelector('#tech-stack'),
+    antialias: true
   });
 const canvasHeight = 500;
 
@@ -31,8 +30,8 @@ renderer.setPixelRatio( window.devicePixelRatio );
 renderer.setSize( window.innerWidth - 80, canvasHeight );
 renderer.shadowMap.enabled = true;
 renderer.shadowMap.type = THREE.PCFSoftShadowMap; 
-camera.position.setZ(5);
-camera.position.setY(2);
+camera.position.setZ(10);
+camera.position.setY(1.5);
 camera.position.setX(0);
 
 const light = new THREE.DirectionalLight(0xffffff, 1, 100);
@@ -43,6 +42,7 @@ light.shadow.mapSize.height = 512; // default
 light.shadow.camera.near = 0.5; // default
 light.shadow.camera.far = 500; // default
 const ambientLight = new THREE.AmbientLight(0xffffff);
+scene.background = new THREE.Color("#dfe2e6"); 
 scene.add(light, ambientLight);
 
 const controls = new OrbitControls(camera, renderer.domElement);
@@ -60,9 +60,6 @@ loadingManager.onLoad = function(){
  }
 
 function animate(){
-    // requestAnimationFrame( animate );
-    // controls.update();
-    // renderer.render( scene, camera );
     requestAnimationFrame((t) => {
         if (previousRAF_ === null) {
           previousRAF_ = t;
@@ -105,33 +102,8 @@ window.addEventListener('DOMContentLoaded', async() => [
         Ammo = lib;
         APP_ = new BasicWorldDemo();
         APP_.initialize();
-
-        //animate();
     })
 ]);
-
-
-class setPhysicsWorld {
-    constructor() {
-
-    }
-}
-
-// const groundGeometry = new THREE.PlaneGeometry( 20, 20, 32, 32 );
-// const groundMaterial = new THREE.MeshStandardMaterial( {color: '#778B70', side: THREE.DoubleSide , roughness: 0.5, metalness: 0 } );
-// const plane = new THREE.Mesh( groundGeometry, groundMaterial );
-// plane.receiveShadow = true;
-// plane.position.y = 0;
-// plane.rotation.x = Math.PI / 2;
-// scene.add( plane );
-
-
-
-// addStack();
-// addLogo();
-
-
-
 class RigidBody{
     constructor(){
 
@@ -159,7 +131,7 @@ class RigidBody{
 
         const btSize = new Ammo.btVector3(size.x * 0.5, size.y * 0.5, size.z * 0.5);
         this.shape_ = new Ammo.btBoxShape(btSize);
-        this.shape_.setMargin(0.05);
+        this.shape_.setMargin(2, 0.05, 0.05);
 
         this.inertia_ = new Ammo.btVector3(0, 0, 0);
         if(mass > 0){
@@ -187,7 +159,6 @@ class RigidBody{
         const btSize = new Ammo.btVector3(size.x * 0.5, size.y * 0.5, size.z * 0.5);
         this.shape_ = new Ammo.btBoxShape(btSize);
         this.shape_.setMargin(0.05);
-
         this.inertia_ = new Ammo.btVector3(0, 0, 0);
         if(mass > 0){
             this.shape_.calculateLocalInertia(mass, this.inertia_);
@@ -220,16 +191,13 @@ class BasicWorldDemo{
         this.solver_ = new Ammo.btSequentialImpulseConstraintSolver();
         this.physicsWorld_ = new Ammo.btDiscreteDynamicsWorld(
             this.dispatcher_, this.broadphase_, this.solver_, this.collisionConfiguration_);
-        this.physicsWorld_.setGravity(new Ammo.btVector3(0, -100, 0));  
+        this.physicsWorld_.setGravity(new Ammo.btVector3(0, -50, 0));  
         
-        this.tmpTransform_ = new Ammo.btTransform();
-
-        const groundGeometry = new THREE.PlaneGeometry( 100, 100, 32, 32 );
-        const groundMaterial = new THREE.MeshStandardMaterial( {color: '#778B70', side: THREE.DoubleSide , roughness: 0.5, metalness: 0 } );
+        const groundGeometry = new THREE.BoxGeometry(100, 1, 100);
+        const groundMaterial = new THREE.MeshStandardMaterial( {color: '#65A87A', roughness: 0.5, metalness: 0 } );
         const plane = new THREE.Mesh( groundGeometry, groundMaterial );
         plane.receiveShadow = true;
-        plane.position.y = 0;
-        plane.rotation.x = Math.PI / 2;
+        plane.position.y = -.5;
         scene.add( plane );
 
         const rbGround = new RigidBody();
@@ -238,113 +206,119 @@ class BasicWorldDemo{
         this.physicsWorld_.addRigidBody(rbGround.body_);
         this.rigidBodies_ = [];
 
+        const stack = [
+            {"tech": ["SQL Server", "Mongo DB","Azure", "Docker"]},
+            {"tech": [".Net Core", "asp.net", "NX", "Angular"]},            
+            {"tech": ["TypeScript", "JavaScript", "HTML", "CSS", "SASS", "C#"]},
+        ];
+        let height = .5;
+        let startXPos = -6;
+        let startYPos = 0;
+        let longestTech = 0;
+        for (let x = 0; x < stack.length; x++){
+            startXPos = startXPos + (longestTech * .5);
+            console.log('Start X pos', startXPos);
+            longestTech = 0;
+            for (let i = 0; i < stack[x].tech.length; i++){
+                const tech = stack[x].tech[i];
+                const techXPos = startXPos;
 
-        const box = new THREE.Mesh(
-            new THREE.BoxGeometry(1, 1, 1),
-            new THREE.MeshStandardMaterial({ color: '#F2630F', roughness: 0.5, metalness: 0 })
-        );
-        box.position.y = 5;
-        box.position.x = 0;
-        box.position.z = -3;
-        box.castShadow = true;
-        box.receiveShadow = true;
-        scene.add(box);
-
-        const rbBox = new RigidBody();
-        rbBox.createBox(1, box.position, box.quaternion, new THREE.Vector3(1, 1, 1));
-        rbBox.setRestitution(0.125);
-        rbBox.setFriction(1);
-        rbBox.setRollingFriction(5);
-        this.physicsWorld_.addRigidBody(rbBox.body_);
-        this.rigidBodies_.push({ mesh: box, rigidBody: rbBox });
-
-
-        const stack = ["HTML", "CSS", "JavaScript"];
-        let startXPos = -3;
-        for (let i = 0; i < stack.length; i++){
-            const loader = new FontLoader(loadingManager);
-            const font = loader.load('../assets/fonts/Heebo Black_Regular.json');
-            loader.load('../assets/fonts/Heebo Black_Regular.json', function(font){
-                const textGeometry = new TextGeometry(stack[i], {
-                    font: font,
-                    size: .5,
-                    depth: .05,
-                    curveSegments: .01,
-                    bevelEnabled: true,
-                    bevelThickness: .0022,
-                    bevelSize: .001,
-                    bevelOffset: 0,
-                    bevelSegments: 1,
+                if (tech.length > longestTech){
+                    longestTech = tech.length;
+                    console.log('Longest tech', longestTech);
+                }
+                const loader = new FontLoader(loadingManager);
+                const font = loader.load('../assets/fonts/Heebo Black_Regular.json');
+                loader.load('../assets/fonts/Heebo Black_Regular.json', function(font){
+                    const textGeometry = new TextGeometry(tech, {
+                        font: font,
+                        size: height,
+                        depth: .1,
+                        curveSegments: .01,
+                        bevelEnabled: true,
+                        bevelThickness: .0022,
+                        bevelSize: .001,
+                        bevelOffset: 0,
+                        bevelSegments: 1,
+                    });
+                    const textMaterial = new THREE.MeshStandardMaterial({color: '#86DFDF', roughness: 0.01, metalness: 0});
+                    const text = new THREE.Mesh(textGeometry, textMaterial);
+                    text.castShadow = true;
+                    text.position.set(techXPos, startYPos, 0);
+                    console.log('Text X pos', techXPos);
+                    text.quaternion.set(0, 0, 0, 1);
+                    scene.add(text);
+    
+                    const rbText = new RigidBody();
+                    rbText.createText(10, text.position, text.quaternion, new THREE.Vector3(.5 * tech.length, .7, 1));
+                    rbText.setRestitution(0.125);
+                    rbText.setFriction(10);
+                    rbText.setRollingFriction(10);
+                    APP_.physicsWorld_.addRigidBody(rbText.body_);
+                    APP_.rigidBodies_.push({ mesh: text, rigidBody: rbText });
                 });
-                const textMaterial = new THREE.MeshStandardMaterial({color: '#86DFDF', roughness: 0.01, metalness: 0});
-                const text = new THREE.Mesh(textGeometry, textMaterial);
-                text.castShadow = true;
-                text.position.y = .5;
-                text.position.x = startXPos;
-                text.position.z = 0;
-                startXPos += (stack[i].length * .5);
-                scene.add(text);
-
-                const rbText = new RigidBody();
-                rbText.createText(1, text.position, text.quaternion, new THREE.Vector3(1, 1, 1));
-                rbText.setRestitution(0.125);
-                rbText.setFriction(1);
-                rbText.setRollingFriction(5);
-                APP_.physicsWorld_.addRigidBody(rbText.body_);
-                APP_.rigidBodies_.push({ mesh: text, rigidBody: rbText });
-            });
+                //startYPos += .6;
             }
-        
-            const loader = new FontLoader(loadingManager);
-            const font = loader.load('../assets/fonts/Heebo Black_Regular.json');
-            loader.load('../assets/fonts/Heebo Black_Regular.json', function(font){
-                const textGeometryTop = new TextGeometry('ET', {
-                    font: font,
-                    size: 2.2,
-                    depth: 1,
-                    curveSegments: .01,
-                    bevelEnabled: true,
-                    bevelThickness: .0022,
-                    bevelSize: .001,
-                    bevelOffset: 0,
-                    bevelSegments: 1,
-                });
-                const textGeometryBottom = new TextGeometry('TECH', {
-                    font: font,
-                    size: 2.2,
-                    depth: 1,
-                    curveSegments: .01,
-                    bevelEnabled: true,
-                    bevelThickness: .0022,
-                    bevelSize: .001,
-                    bevelOffset: 0,
-                    bevelSegments: 1,
-                });
-                const textMaterial = new THREE.MeshStandardMaterial({color: '#F2630F', roughness: 0.01, metalness: 0});
-                const text = new THREE.Mesh(textGeometryTop, textMaterial);
-                const textBottom = new THREE.Mesh(textGeometryBottom, textMaterial);
-                textBottom.castShadow = true;
-                textBottom.position.y = 0;
-                textBottom.position.x = -10;
-                textBottom.position.z = -5;
-                textBottom.rotation.y = Math.PI / 5;
-                scene.add(textBottom);
-                text.castShadow = true;
-                text.position.y = 2.2;
-                text.position.x = -10;
-                text.position.z = -5;
-                text.rotation.y = Math.PI / 5;
-                scene.add(text);
-            });
+        }
 
-            animate();
+        const loader = new FontLoader(loadingManager);
+        const font = loader.load('../assets/fonts/Heebo Black_Regular.json');
+        loader.load('../assets/fonts/Heebo Black_Regular.json', function(font){
+            const textGeometryTop = new TextGeometry('ET', {
+                font: font,
+                size: 3,
+                depth: 1,
+                curveSegments: .01,
+                bevelEnabled: true,
+                bevelThickness: .0022,
+                bevelSize: .001,
+                bevelOffset: 0,
+                bevelSegments: 1,
+            });
+            const textGeometryBottom = new TextGeometry('TECH', {
+                font: font,
+                size: 3,
+                depth: 1,
+                curveSegments: .01,
+                bevelEnabled: true,
+                bevelThickness: .0022,
+                bevelSize: .001,
+                bevelOffset: 0,
+                bevelSegments: 1,
+            });
+            const textMaterial = new THREE.MeshStandardMaterial({color: '#F2630F', roughness: 0.01, metalness: 0});
+            const text = new THREE.Mesh(textGeometryTop, textMaterial);
+            const textBottom = new THREE.Mesh(textGeometryBottom, textMaterial);
+            textBottom.castShadow = true;
+            textBottom.position.y = 0;
+            textBottom.position.x = -18;
+            textBottom.position.z = -6;
+            textBottom.rotation.y = Math.PI / 5;
+            scene.add(textBottom);
+            text.castShadow = true;
+            text.position.y = 3;
+            text.position.x = -18;
+            text.position.z = -6;
+            text.rotation.y = Math.PI / 5;
+            scene.add(text);
+        });
+
+
+        this.tmpTransform_ = new Ammo.btTransform();
+
+        this.countdown_ = 1.0;
+        this.count_ = 0;
+        this.previousRAF_ = null;
+
+        animate();
     }
 
     step_(timeElapsed){
         const timeElapsedS = timeElapsed * 0.001;
 
         this.countdown_ -= timeElapsedS;
-        if (this.countdown_ < 0 && this.count_ < 10) {
+        if (this.countdown_ < 0 && this.count_ < 12) {
+            console.log('Spawn', this.countdown_);
           this.countdown_ = 0.25;
           this.count_ += 1;
           this.spawn_();
@@ -363,78 +337,31 @@ class BasicWorldDemo{
           this.rigidBodies_[i].mesh.quaternion.copy(quat3);
         }
     }
-
-
-    //  addStack(){
-    //     for (let i = 0; i < stack.length; i++){
-    //         const loader = new FontLoader(loadingManager);
-    //         const font = loader.load('../assets/fonts/Heebo Black_Regular.json');
-    //         loader.load('../assets/fonts/Heebo Black_Regular.json', function(font){
-    //             const textGeometry = new TextGeometry(stack[i], {
-    //                 font: font,
-    //                 size: .5,
-    //                 depth: .05,
-    //                 curveSegments: .01,
-    //                 bevelEnabled: true,
-    //                 bevelThickness: .0022,
-    //                 bevelSize: .001,
-    //                 bevelOffset: 0,
-    //                 bevelSegments: 1,
-    //             });
-    //             const textMaterial = new THREE.MeshStandardMaterial({color: '#86DFDF', roughness: 0.01, metalness: 0});
-    //             const text = new THREE.Mesh(textGeometry, textMaterial);
-    //             text.castShadow = true;
-    //             text.position.y = .5;
-    //             text.position.x = this.startXPos;
-    //             text.position.z = 0;
-    //             startXPos += (stack[i].length * .5);
-    //             scene.add(text);
-    //         });
-    //     }
-    // }
+    spawn_() {
+        const scale = Math.random() * .7 + .7;
+        const box = new THREE.Mesh(
+          new THREE.BoxGeometry(scale, scale, scale),
+          new THREE.MeshStandardMaterial({
+              color: '#F2630F',
+              roughness: 0.5, 
+              metalness: 0
+          }));
+        box.position.set(Math.random() * 10 - 5, 200.0, Math.random() * 2 - 1);
+        box.quaternion.set(0, 0, 0, 1);
+        box.castShadow = true;
+        box.receiveShadow = true;
     
-    // addLogo(){
-    //     const loader = new FontLoader(loadingManager);
-    //     const font = loader.load('../assets/fonts/Heebo Black_Regular.json');
-    //     loader.load('../assets/fonts/Heebo Black_Regular.json', function(font){
-    //         const textGeometryTop = new TextGeometry('ET', {
-    //             font: font,
-    //             size: 2.2,
-    //             depth: 1,
-    //             curveSegments: .01,
-    //             bevelEnabled: true,
-    //             bevelThickness: .0022,
-    //             bevelSize: .001,
-    //             bevelOffset: 0,
-    //             bevelSegments: 1,
-    //         });
-    //         const textGeometryBottom = new TextGeometry('TECH', {
-    //             font: font,
-    //             size: 2.2,
-    //             depth: 1,
-    //             curveSegments: .01,
-    //             bevelEnabled: true,
-    //             bevelThickness: .0022,
-    //             bevelSize: .001,
-    //             bevelOffset: 0,
-    //             bevelSegments: 1,
-    //         });
-    //         const textMaterial = new THREE.MeshStandardMaterial({color: '#F2630F', roughness: 0.01, metalness: 0});
-    //         const text = new THREE.Mesh(textGeometryTop, textMaterial);
-    //         const textBottom = new THREE.Mesh(textGeometryBottom, textMaterial);
-    //         textBottom.castShadow = true;
-    //         textBottom.position.y = 0;
-    //         textBottom.position.x = -10;
-    //         textBottom.position.z = -5;
-    //         textBottom.rotation.y = Math.PI / 5;
-    //         scene.add(textBottom);
-    //         text.castShadow = true;
-    //         text.position.y = 2.2;
-    //         text.position.x = -10;
-    //         text.position.z = -5;
-    //         text.rotation.y = Math.PI / 5;
-    //         scene.add(text);
-    //     });
-    // }
+        const rb = new RigidBody();
+        rb.createBox(DEFAULT_MASS, box.position, box.quaternion, new THREE.Vector3(scale, scale, scale), null);
+        rb.setRestitution(0.15);
+        rb.setFriction(2);
+        rb.setRollingFriction(1);
+    
+        this.physicsWorld_.addRigidBody(rb.body_);
+    
+        this.rigidBodies_.push({mesh: box, rigidBody: rb});
+    
+        scene.add(box);
+      }
 }
 
