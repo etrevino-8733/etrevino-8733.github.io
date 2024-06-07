95e3e034f27966e825b2851c932eb1cce922b0ab
Emmanuel Trevino <etrevino8733@gmail.com>
2024-06-07 15:00:21 -0500

    UX updates

--- scripts.js
+++ scripts.js
@@ -16,24 +16,3 @@ function isMobile() {
 
   }
 
-let welcomeMessage;
-window.onload = async function(){
-    welcomeMessage = document.getElementById('welcome-message');
-
-    myMessages = ['WELCOME!', 'USE CHROME FOR BEST EXPERIENCE', 'GETTING THINGS READY, PLEASE BE PATIENT...'];
-
-    for(let i = 0; i < myMessages.length; i++){
-        setTimeout(function(){
-            welcomeMessage.style.opacity = 1;
-            welcomeMessage.innerHTML = myMessages[i];
-        }, i * 5000);
-
-        if(i < myMessages.length - 1){
-            setTimeout(function(){
-                welcomeMessage.style.opacity = 0;
-            }
-            , i * 5000 + 3000);
-        }
-
-    }
-}
